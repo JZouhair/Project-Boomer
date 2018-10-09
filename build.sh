ba7481f52b43ccc67a1f81919d465ba5d1a6abb1
zouhair <jawadi.zouhair.2017@gmail.com>
2018-10-09 14:18:49 +0100

    [build] - modified tests2

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
     echo 'Checking Quality Gates'
-    mvn -B clean verify sonar:sonar  -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=boomerkey -Dsonar.organization=jzouhair-github -Dsonar.login=583a3428dfa8f7a98ee213e22aa609dd1ad6db24;
+    mvn -B clean verify sonar:sonar  -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=boomerkey  -Dsonar.organization=jzouhair-github -Dsonar.login=53f75ec996b4bd6151b4301bb93ce24b4cbb061a;
 fi
 
 
