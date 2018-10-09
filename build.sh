7b9966761c0dd1d1eae120436a1f3bd038d470fa
zouhair <jawadi.zouhair.2017@gmail.com>
2018-10-09 10:38:56 +0100

    commit project boomer test1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
     echo 'Checking Quality Gates'
-    mvn -B clean verify sonar:sonar  -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=JZouhair_sonarCloud-Test -Dsonar.organization=JZouhair-github -Dsonar.login=3acb4bc4935c13818fb1e43374dea683e06eaca9;
+    mvn -B clean verify sonar:sonar  -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=boomerkey -Dsonar.organization=jzouhair-github -Dsonar.login=583a3428dfa8f7a98ee213e22aa609dd1ad6db24;
 fi
 
 
