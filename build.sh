b28509e2c7053b09ff2d01d7d6c87a84734df2e1
zouhair <jawadi.zouhair.2017@gmail.com>
2018-10-10 12:01:03 +0100

    Make build.sh executable

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
     echo 'Checking Quality Gates'
-    mvn -B clean verify sonar:sonar 
+    mvn -B clean verify sonar:sonar\
      -Dsonar.host.url=${SONAR_URL}\ 
      -Dsonar.projectKey=${SONAR_PROJECT}\ 
      -Dsonar.organization= ${SONAR_ORGANIZATION}\ 
